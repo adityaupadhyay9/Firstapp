5f36231a1df8cb64f714d252a39429246ac30da0
Aditya <adityau339@gmail.com>
2020-10-02 15:12:31 +0530

    Changes in Both activity have been made

--- FragmentExample/app/src/main/java/com/app/fragmentexample/FragmentFirst.kt
+++ FragmentExample/app/src/main/java/com/app/fragmentexample/FragmentFirst.kt
@@ -1,17 +1,24 @@
 package com.app.fragmentexample
 
 import android.os.Bundle
+import android.provider.DocumentsContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.*
 
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 
+
 class FragmentFirst : Fragment() {
+
+    lateinit var textview:TextView
 
     private var param1: String? = null
     private var param2: String? = null
@@ -21,15 +28,28 @@ class FragmentFirst : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
+
         }
     }
 
     override fun onCreateView(
+
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        var bundle:Bundle=
+
+
+        var bundle: Bundle? =this.arguments
+        if (bundle != null) {
+                var str:String=bundle.getString("value1",null)
+                Toast.makeText(context,str,Toast.LENGTH_SHORT).show()
+
+
+        }
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
--- FragmentExample/app/src/main/java/com/app/fragmentexample/MainActivity.kt
+++ FragmentExample/app/src/main/java/com/app/fragmentexample/MainActivity.kt
@@ -5,25 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btn:Button=findViewById(R.id.btn1)
-        var rd:EditText=findViewById(R.id.edittext1)
+        var ed:EditText=findViewById(R.id.edittext1)
+
+
 
         btn.setOnClickListener(){
 
-            var i:Intent= Intent(this,FragmentFirst::class.java)
-            i.putExtra("value",edittext1.text.toString())
+            var bundle:Bundle=Bundle()
+            bundle.putString("value1",ed.text.toString())
 
             val firstfragment:FragmentFirst= FragmentFirst()
             val manager=supportFragmentManager
             val transaction=manager.beginTransaction()
 
+           firstfragment.arguments=bundle
             transaction.replace(R.id.fragmentcontainer,firstfragment)
             transaction.commit()
         }
